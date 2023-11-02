701491f3b2f60a04cee48ef3cd35dbb261adaee6
masterblaster42069 <114529314+masterblaster42069@users.noreply.github.com>
2023-11-02 14:40:21 +0100

    fawfaw
    afwfawfaw

--- view.js
+++ view.js
@@ -5,14 +5,16 @@ function displayResult() {
     <li id="list">
         <img src="${games.thumb}">
         <p id="title">Title: ${games.title}</p>
-        <p id="price">Price: ${games.salePrice}</p>
-        <p id="normal">Normal Price: ${games.normalPrice}</p>
+        <p id="price">Price: $${games.salePrice}</p>
+        <p id="normal">Normal Price: $${games.normalPrice}</p>
         <p id="sale">${games.isOnSale ? "On Sale" : "No Sale Here"}</p>
         <p id="sRating">SteamRating: ${games.steamRatingText}</p>
         <p id="mRating">Metacritic: ${games.metacriticScore}</p>
+        <p id="rating">Deal Rating: ${games.dealRating}</p>
         <a id="link" href="https://store.steampowered.com/app/${
             games.steamAppID
         }">Steam</a>
+
     </li>
         `;
         }
